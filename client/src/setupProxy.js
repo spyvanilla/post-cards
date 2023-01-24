1b8aebfd8685e3cb0959c8efaa71509c6e7a0101
spyvanilla <levisbarros1234@gmail.com>
2023-01-24 10:30:23 -0300

    put proxy url in the client for an environment variable to separate the dev build from the prod buil

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,10 +1,12 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+import {createProxyMiddleware} from 'http-proxy-middleware';
 
-module.exports = function(app) {
+const proxyUrl = process.env.REACT_APP_PROXY_URL;
+
+export default function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://127.0.0.1:5000',
+      target: proxyUrl,
       changeOrigin: true,
     })
   );
